2d69c49bdb207d08c98c25a85f84b8000d6fef36
unknown <danielthegreyt@gmail.com>
2025-11-20 12:48:57 +0100

    add favicon

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,8 @@ export default function App({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0" />
         <meta name="description" content="Derby Kids Online School - Quality online education for children worldwide" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="apple-touch-icon" href="/favicon.svg" />
       </Head>
       <Layout>
         <Component {...pageProps} />
