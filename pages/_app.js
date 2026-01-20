51a0c222fd8dd9e1f45d3bca7383ca8f91f8f6ea
unknown <danielthegreyt@gmail.com>
2026-01-20 14:37:01 +0100

    Add Facebook Pixel tracking (ID: 2045998496196809)

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,33 @@
 import React, { useState, useEffect } from 'react'
 import Head from 'next/head'
+import Script from 'next/script'
+import { useRouter } from 'next/router'
 import '@/styles/globals.css'
 import Layout from '@/Layout'
 import SplashScreen from '@/components/SplashScreen'
 import { LanguageProvider } from '@/lib/LanguageContext'
 
+// Facebook Pixel ID
+const FB_PIXEL_ID = '2045998496196809'
+
 export default function App({ Component, pageProps }) {
   const [showSplash, setShowSplash] = useState(true);
   const [splashComplete, setSplashComplete] = useState(false);
+  const router = useRouter();
+
+  // Facebook Pixel - Track page views on route change
+  useEffect(() => {
+    const handleRouteChange = () => {
+      if (typeof window !== 'undefined' && window.fbq) {
+        window.fbq('track', 'PageView');
+      }
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
 
   useEffect(() => {
     // Check if splash has been shown this session
@@ -29,6 +49,34 @@ export default function App({ Component, pageProps }) {
 
   return (
     <LanguageProvider>
+      {/* Facebook Pixel Script */}
+      <Script
+        id="fb-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${FB_PIXEL_ID}');
+            fbq('track', 'PageView');
+          `,
+        }}
+      />
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: 'none' }}
+          src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+          alt=""
+        />
+      </noscript>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0" />
         <meta name="description" content="Derby Kids Online School - Quality online education for children worldwide" />
